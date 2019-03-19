984be1b797ce7384dcf68f03b5786c3f9e177277
fmtitov <36996070+fmtitov@users.noreply.github.com>
2019-03-19 13:58:49 -0500

    Add files via upload

--- public/list.js
+++ public/list.js
@@ -1,5 +1,5 @@
 function getPList(page) {
     //get the id of the selected table from the filter dropdown
     var person_id = document.getElementById('getList').value
-    window.location = '/' + page + '/list/' +  encodeURI(person_id)
+    window.location = '/passenger-pilot/' + page + '/' +  encodeURI(person_id)
 }
